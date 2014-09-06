c9d8f3f9b2eb88c26739e65668f53a53a008a9a1
LisaStoz <lisa@lisastoz.com>
2014-09-06 18:30:01 +0100

    example implementation

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // Let's write application code here
+
+function registrationMsg(days) {
+  if (days > 15) {
+    return 'Registration opens soon';
+  } else if (days > 0) {
+    if (days === 1) {
+      return 'You have 1 day left to register';
+    } else {
+      return 'You have ' + days + ' days left to register';
+    }
+  }
+  return '';
+}
