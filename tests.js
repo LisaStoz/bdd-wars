97a874c3cfad79b7d969f2652209e434c1ec75bf
LisaStoz <lisa@lisastoz.com>
2014-09-06 18:15:06 +0100

    example tests

--- tests.js
+++ tests.js
@@ -1,1 +1,25 @@
 // Let's write tests here
+
+describe("When a registration", function(){
+
+  it("ends in 15 days, the output is 'You have 15 days left to register'", function(){
+    expect(countdownMessage(15)).toBe("You have 15 days left to register");
+  });
+
+  it("ends in 3 days, the output is 'You have 3 days left to register'", function(){
+    expect(countdownMessage(3)).toBe("You have 3 days left to register");
+  });
+
+  it("ends in 1 day, the output is 'You have 1 day left to register'", function(){
+    expect(countdownMessage(1)).toBe("You have 1 day left to register");
+  });
+
+  it("ends the output is 'Registation is closed'", function(){
+    expect(countdownMessage(0)).toBe("Registation is closed");
+  });
+
+  it("is not open yet - nothing is displayed", function(){
+    expect(countdownMessage(16)).toBe("");
+  });
+
+});
