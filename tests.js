2c02c2600c944294535a5baf992e3d57a4a474f0
LisaStoz <lisa@lisastoz.com>
2014-09-06 18:24:54 +0100

    better simpler example tests

--- tests.js
+++ tests.js
@@ -1,25 +1,25 @@
 // Let's write tests here
 
-describe("When a registration", function(){
+describe("When registration", function(){
 
-  it("ends in 15 days, the output is 'You have 15 days left to register'", function(){
-    expect(countdownMessage(15)).toBe("You have 15 days left to register");
+  it("ends in 15 days, then a countdown message is displayed", function(){
+    expect(registrationMsg(15)).toBe("You have 15 days left to register");
   });
 
-  it("ends in 3 days, the output is 'You have 3 days left to register'", function(){
-    expect(countdownMessage(3)).toBe("You have 3 days left to register");
+  it("ends in 3 days, then a countdown message is displayed", function(){
+    expect(registrationMsg(3)).toBe("You have 3 days left to register");
   });
 
-  it("ends in 1 day, the output is 'You have 1 day left to register'", function(){
-    expect(countdownMessage(1)).toBe("You have 1 day left to register");
+  it("ends in 1 day, then a countdown message is displayed", function(){
+    expect(registrationMsg(1)).toBe("You have 1 day left to register");
   });
 
-  it("ends the output is 'Registation is closed'", function(){
-    expect(countdownMessage(0)).toBe("Registation is closed");
+  it("is over, then nothing is displayed", function(){
+    expect(registrationMsg(0)).toBe("");
   });
 
-  it("is not open yet - nothing is displayed", function(){
-    expect(countdownMessage(16)).toBe("");
+  it("is not open yet, then opens soon message is displayed", function(){
+    expect(registrationMsg(16)).toBe("Registration opens soon");
   });
 
 });
